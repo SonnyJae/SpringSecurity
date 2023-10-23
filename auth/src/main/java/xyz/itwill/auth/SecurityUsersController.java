b16f43b328da27cf22a45305caa0d5560f568c20
HwiJae <gr6064@gmail.com>
2023-10-23 14:24:39 +0900

    SpringSecurity+OAuth

---
+++ auth/src/main/java/xyz/itwill/auth/SecurityUsersController.java
@@ -0,0 +1,62 @@
+package xyz.itwill.auth;
+
+import javax.validation.Valid;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import lombok.RequiredArgsConstructor;
+import xyz.itwill.dto.SecurityAuth;
+import xyz.itwill.dto.SecurityUsers;
+import xyz.itwill.dto.SecurityUsersGroups;
+import xyz.itwill.service.SecurityUsersService;
+
+@Controller
+@RequiredArgsConstructor
+public class SecurityUsersController {
+	private final SecurityUsersService securityUsersService;
+	private final PasswordEncoder passwordEncoder;
+	
+	@RequestMapping(value="/security/add", method = RequestMethod.GET)
+	@ResponseBody
+	public String addUsers() {
+		SecurityUsers user1=new SecurityUsers("abc123", 
+				passwordEncoder.encode("123456"), "홍길동", "abc@itwill.xyz", "1", null);
+		SecurityUsers user2=new SecurityUsers("xyz789", 
+				passwordEncoder.encode("123456"), "임꺽정", "xyz@itwill.xyz", "1", null);
+		SecurityUsers user3=new SecurityUsers("opq456", 
+				passwordEncoder.encode("123456"), "전우치", "opq@itwill.xyz", "1", null);
+		
+		securityUsersService.addSecurityUsers(user1);
+		securityUsersService.addSecurityUsers(user2);
+		securityUsersService.addSecurityUsers(user3);
+		
+		SecurityAuth auth1=new SecurityAuth("abc123", "ROLE_USER");
+		SecurityAuth auth2=new SecurityAuth("abc123", "ROLE_MANAGER");
+		SecurityAuth auth3=new SecurityAuth("xyz789", "ROLE_MANAGER");
+		SecurityAuth auth4=new SecurityAuth("opq456", "ROLE_ADMIN");
+
+		securityUsersService.addSecurityAuth(auth1);
+		securityUsersService.addSecurityAuth(auth2);
+		securityUsersService.addSecurityAuth(auth3);
+		securityUsersService.addSecurityAuth(auth4);
+		
+		return "success";
+	}
+	
+	@RequestMapping(value="/security/add", method = RequestMethod.POST)
+	public String addUsers(@ModelAttribute @Validated(SecurityUsersGroups.insertValid.class) SecurityUsers users, BindingResult bindingResult) {
+		if(bindingResult.hasErrors()) {
+			return "join";
+		}
+		return "redirect:/login";
+	}
+}
---
+++ auth/src/main/java/xyz/itwill/dto/SecurityUsers.java
@@ -0,0 +1,36 @@
+package xyz.itwill.dto;
+
+import java.util.List;
+
+import javax.validation.constraints.NotEmpty;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/*
+create table security_users(userid varchar2(100) primary key, passwd varchar2(100)
+    , name varchar2(50), email varchar2(100), enabled varchar2(1));
+    
+create table security_auth(userid varchar2(100), auth varchar2(50)
+    , CONSTRAINT auth_userid_fk foreign key(userid) references security_users(userid));
+    
+create unique index auth_userid_index on security_auth(userid, auth);   
+*/
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SecurityUsers {
+	@NotEmpty(groups = {SecurityUsersGroups.insertValid.class})
+	private String userid;
+	@NotEmpty(groups = {SecurityUsersGroups.insertValid.class})
+	private String passwd;
+	@NotEmpty(groups = {SecurityUsersGroups.insertValid.class, SecurityUsersGroups.modifyValid.class})
+	private String name;
+	@NotEmpty(groups = {SecurityUsersGroups.insertValid.class, SecurityUsersGroups.modifyValid.class})
+	private String email;
+	@NotEmpty(groups = {SecurityUsersGroups.modifyValid.class})
+	private String enabled;
+	private List<SecurityAuth> securityAuthList;
+}
